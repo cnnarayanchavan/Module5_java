7a0025907b5c226859c5ecf76431c829ddcd165e
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-17 11:53:20 +0530

    Create Narayana1.java_Revides Array_list in java take_1

---
+++ Narayana1.java
@@ -0,0 +1,13 @@
+// Arraylist
+
+public class Narayana1{
+  public static void main(String[] args){
+    Arraylist<Integer> Nums = new Arraylist<Integer>;
+    Nums.add(12);
+    Nums.add(13);
+    Nums.add(14);
+    Nums.add(15);
+    Nums.add(16);
+    System.out.println(Nums);
+  }
+}
