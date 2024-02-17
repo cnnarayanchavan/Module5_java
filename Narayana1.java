970618bf1d44cdff789bb616ebbbe122b0c6e0fc
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-17 12:01:14 +0530

    Update Narayana1.java_revised arraylist in java Take_2

--- Narayana1.java
+++ Narayana1.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 public class Narayana1{
   public static void main(String[] args){
-    ArrayList<Integer> Nums = new ArrayList<Integer>;
+    ArrayList<Integer> Nums = new ArrayList<Integer>();
     Nums.add(12);
     Nums.add(13);
     Nums.add(14);
