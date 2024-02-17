7bc6b7b096798dcef0c17fcf68dad978e1f0209a
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-17 12:07:44 +0530

    Revise arrarlist in java take_6

--- Narayana1.java
+++ Narayana1.java
@@ -9,6 +9,11 @@ public class Narayana1{
     Nums.add(15);
     Nums.add(16);
     System.out.println(Nums);
+    for (int i = 0; i < Nums.size(); i++) {
+      System.out.println(Nums.get(i));
+    }
+    System.out.println(Nums.get(3));
   }
+
 }
 //_revised arraylist in java Take_2
