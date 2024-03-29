1763516f78a5571260c503df0665ae3492328f6a
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-29 10:02:12 +0530

    Create prime.java_another 001

---
+++ prime.java
@@ -0,0 +1,21 @@
+//the number which has exact tow factors ("PRIME NUMBER")
+// one and the number itself
+java.util.*;
+public class prime{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number : ");
+    int num = sc.nextInt();
+    for(int i=0;  i<=num;  i++){
+      int count =0;
+      if(num%i==0){
+        count++;
+      }
+      if(count==2){
+        System.out.println("Prime number");
+      }
+      else System.out.println("Not prime number");
+    }
+      
+  }
+}
