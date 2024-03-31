5148ef2a590cb21b65b970adf31195a985f9efee
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-31 14:46:51 +0530

    Update que1LinSear.java_forgot main class

--- que1LinSear.java
+++ que1LinSear.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
 public class que1LinSear{
+  public static void main(String[]args){
   int[] Array1 = {12,15,161,202,149};
   for(int i =0; i<Array1.length; i++){
       String intToStr = Integer.toString(i);
   }
 System.out.println("Length of an string is" + intToStr.length());
+  }
 }
