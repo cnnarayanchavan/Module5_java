26a347824d9de339eb415a387d09bc5bb0893ee5
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-31 14:43:19 +0530

    Create que1LinSear.java_checking first condtion

---
+++ que1LinSear.java
@@ -0,0 +1,7 @@
+public class que1LinSear{
+  int [] Array1 = {12,15,161,202,149};
+  for(int i =0; i<Array1.length; i++){
+      String intToStr = Interger.toString(i);
+  }
+System.out.println("Length of an string is" + intToStr.length());
+}
