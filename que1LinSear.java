f57449a72ae23358e632a882088606d0cba946a6
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-03-31 14:56:58 +0530

    First question done leetcode que no:1295

--- que1LinSear.java
+++ que1LinSear.java
@@ -2,9 +2,11 @@ import java.util.Arrays;
 public class que1LinSear{
   public static void main(String[]args){
   int[] Array1 = {12,15,161,202,149};
-  for(int i =0; i<Array1.length; i++){
-      String intToStr = Integer.toString(i);
+  for(int i = 0; i<Array1.length; i++){
+      String int_to_str = Integer.toString(Array1[i]);
+      if(int_to_str.length()%2==0){
+        System.out.print(Array1[i] + " ");
+      }
   }
-System.out.println("Length of an string is" + intToStr.length(i));
   }
 }
