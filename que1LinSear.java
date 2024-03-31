58bc5297ee55950f2c91d861db8cb5bb097022d4
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-03-31 15:05:08 +0530

    missed the condition

--- que1LinSear.java
+++ que1LinSear.java
@@ -1,13 +1,15 @@
 import java.util.Arrays;
 public class que1LinSear{
   public static void main(String[]args){
+    int count = 0;
   int[] Array1 = {12,15,161,202,149};
   for(int i = 0; i<Array1.length; i++){
       String int_to_str = Integer.toString(Array1[i]);
       if(int_to_str.length()%2==0){
-        System.out.print(Array1[i] + " ");
+        count = count+1;
       }
     }
+    System.out.println("Total number with even digits in the array are : " +count);    
   }
 }
 
