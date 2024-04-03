4f33ce79d0d2997d309450035a4353ce5c52295d
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-03 12:34:26 +0530

    Update binarySearchDSA.java_first solved_(find target element)

--- binarySearchDSA.java
+++ binarySearchDSA.java
@@ -1,1 +1,25 @@
+public class binarySearchDSA{
+  public static void main(String[]args){
+    int[] arr = {2,8,16,18,19,20,22,25};
+    int Target = 16;  
+  }
+  static int binarySer(int[] Array, targetEle){
+    int startInd = 0;
+    int endInd = Array.length-1 
+    while(start<=endInd){
+      int midInd = startInd+(endInd-startInd)/2;
+      if(targetEle > Array[midInd]){
+        startInd = midInd + 1; 
+      }
+      else if(targetEle < Array[midInd]){
+        startInd = midInd - 1;
+      }
+      else{
+        return midInd;
+      }
+      return "Element not found, enter valid element"
+    }
+  }
+}
 
+  
