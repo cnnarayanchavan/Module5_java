e3fc8c033f0112140560a9d2a42485c1a969133f
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-04 05:46:15 +0530

    Create binarySearchDSA_Desc.java_searching in descending array (Binary search)

---
+++ binarySearchDSA_Desc.java
@@ -0,0 +1,27 @@
+public class binarySearchDSA_Desc{
+  public static void main(String[] args){
+    int arr[] = {72,68,58,48,38,27,22,18,15,5};
+    int target = 22;
+    int op = searchInDes(arr,target);
+    system.out.println(op);
+    
+    
+  }
+  static int searchInDes(int[] arrr,int tar){
+    int start = 0;
+    int end = arrr.length-1;
+    while(start <= end){
+      int mid = start+(end-start)/2;
+      if(tar < arrr[mid]){
+        start = mid + 1;
+      }
+      if(tar < arrr[mid]){
+        end = mid - 1;
+      }
+      else(tar==arrr[mid]){
+        return arrr[mid];
+      }
+    }
+    return -1;  //if element is not found
+  }
+}
