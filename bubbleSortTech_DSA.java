604596fa408d302b3aec95c4efba0806e237ce74
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-05 05:41:45 +0530

    Update bubbleSortTech_DSA.java

--- bubbleSortTech_DSA.java
+++ bubbleSortTech_DSA.java
@@ -20,7 +20,7 @@
 import java.util.Arrays;
  public class bubbleSortTech_DSA {
    public static void main(String[]args){
-     int[] arr = {5,4,6,88,9,7,90,5,};
+     int[] arr = {5,4,6,88,9,7,90,5};
       for(int i = 0; i<arr.length; i++){
       for(int j = 1; j<arr.length-1; j++){  // here with every iteration the last element in array will be sorted so no need to go on that index 
          if(arr[j]<arr[j-1]){
