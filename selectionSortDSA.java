a4afe3b09607dd5756037b4b11fd3fe83fa78961
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-06 17:41:49 +0530

    Update selectionSortDSA.java

--- selectionSortDSA.java
+++ selectionSortDSA.java
@@ -2,10 +2,10 @@ public class selectionSortDSA{
   public static void main(String[]args){
   int[]Arr1 = {5,4,6,7,9,2};
   int ans = findMax(Arr1);
-    System.out.println(ans);
+  System.out.println(ans);
   }
   public static int findMax(int[] arr){
-    for(i = 0; i<arr.length; i++){
+    for(int i = 0; i<arr.length; i++){
       int max = 0;
       if(max<arr[i]){
         max = arr[i];
