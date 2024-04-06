aa030fc7600943b62d9efb1854da9835a7ba30ec
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-06 19:43:36 +0530

    Update selectionSortDSA.java

--- selectionSortDSA.java
+++ selectionSortDSA.java
@@ -1,35 +1,15 @@
-import java.util.Arrays;
 public class selectionSortDSA{
-  public static void main(String[]args){
-  int[]Arr1 = {5,4,6,7,9,2};
-  // int ans = findMax(Arr1);
-  // System.out.println(ans);
-    int max = 0;
-    int end = Arr1.length-1;
-    int temp;
-    for(int i = 0; i<Arr1.length-1; i++){
-      if(max<Arr1[i]){
-        max = i;
-        temp = max;
-        max = end;
-        end = temp;
+  publci static void main(String[]args){
+    int[] arr = {4,5,6,7,12};
+    for(int i = 0; i<arr.length-1; i++){
+      int max = 0;
+      if(max<arr[i]){
+        max= arr[i];
       }
     }
-    System.out.println(Arrays.toString(Arr1));
+    System.ot.println(max);
   }
-  // public static int findMax(int[] arr){
-  //   int max = 0;
-  //   for(int i = 0; i<arr.length; i++){
-  //     if(max<arr[i]){
-  //       max = arr[i];
-  //     }
-  //   }
-  //    return max;
-  // }
-  // public static void swapElement(){
-  //   for(int i = 0; i<arr.length-i-1; i++){
-      
-  //   }
-  // }
 }
+
+
 
