4a718e48cdafba36e52c09fe833ed7efd0b28037
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-04-07 18:10:45 +0530

    Done

--- selectionSortDSA.java
+++ selectionSortDSA.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class selectionSortDSA{
   public static void main(String[]args){
-    int[] arr = {7,4,9,2,10};
+    int[] arr = {9,8,7,6,5};
     for(int i = 0; i<arr.length; i++){
     int max = 0;
     int end = arr.length - i - 1;
