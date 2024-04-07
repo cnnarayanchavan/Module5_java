e0b66cc71d872ac39731f4837eb979ef8e85d3d7
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-04-07 17:43:44 +0530

    Update selectionSortDSA.java take2_Selection sort

--- selectionSortDSA.java
+++ selectionSortDSA.java
@@ -3,15 +3,15 @@ public class selectionSortDSA{
   public static void main(String[]args){
     int[] arr = {7,4,9,2,10};
     for(int i = 0; i<arr.length; i++){
-      int max = 0;
-      int end = arr.length-1;
-      for(int j = 0; j<=end; j++){
-      if(max<arr[j]){
+    int max = 0;
+    int end = arr.length - i - 1;
+    if(arr[max]<arr[i]){
         max = i;
       }
-      int temp = arr[max];
-      arr[max] = arr[end];
-      arr[end] = temp;
+    for(int j = 0; j<=end; j++){
+    int temp = arr[max];
+    arr[max] = arr[end];
+    arr[end] = temp;
       }
     }
     System.out.println(Arrays.toString(arr));
